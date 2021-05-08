1ae95227405a1d1ca9a9ca8382d9a5bcf0a4b2c6
Tejas Rajput <tejasonweb@gmail.com>
2021-05-08 22:00:54 +0530

    added error hadling

--- VaccineNotifier.py
+++ VaccineNotifier.py
@@ -4,6 +4,7 @@ import time
 import json
 
 def getVaccineSlots(date,pincode):
+    print("Pin : " + str(pincode) + " and Date : Next 10 days")
     url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode="+str(pincode)+"&date="+date
     headers = {'Authority':'cdn-api.co-vin.in',
     'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
@@ -11,7 +12,7 @@ def getVaccineSlots(date,pincode):
     if vreq.status_code == 200:
         centers = vreq.json()["centers"]
         if len(centers)>0:
-            print(str(len(centers)) + " centers are available.")
+            #print(str(len(centers)) + " center(s) are available.")
             for c in range(len(centers)):
                 #check if there if capacity > 0 for the center
                 sessions = centers[c]["sessions"]
@@ -31,19 +32,25 @@ def getVaccineSlots(date,pincode):
                     print(str(len(slots)) +" slots are available")
                     for i in range(len(slots)):
                         print(slots[i])
-                else:
-                    print("No capacity is available at center : "+centers[c]["name"] + " for date : "+sessions[0]["date"])
-                print("=============================================================")
-        else:
-            print("No centers available for the pin code : "+str(pincode)+ " and date : "+str(date))
+                #else:
+                    #print("No capacity is available at center : "+centers[c]["name"] + " for date : "+sessions[0]["date"])
+        #else:
+            #print("No centers available for the pin code : "+str(pincode))
     else:
-        print("error : "+str(vreq.status_code) + ". Try again later.")
+        print("error : "+str(vreq.status_code) + ".")
+        if(vreq.status_code==403):
+            print("Error 403 could be due to too many requests. Will try again in 60 seconds.")
+            time.sleep(60)
+            getVaccineSlots(date,pincode)
 
-pincode = 411027
+pincode = 411001
 date1 = datetime.datetime.now().strftime("%d-%m-%Y")
+date2 = str(datetime.datetime.strptime("05-05-2021","%d-%m-%Y").strftime("%d-%m-%Y"))
+# open pincode file
+pincodefile = open("PunePinCodeList.txt")
 
-for i in range(998):
-    print("Getting Vaccine slots for Pin : " + str(pincode) + " and Date : " + date1)
-    getVaccineSlots(date1,pincode)
-    pincode = pincode + 1
-    
+for pincode in pincodefile:
+    #print("=============================================================")
+    getVaccineSlots(date1,pincode.strip())
+    #time.sleep(0)
+print("=============================================================")    
